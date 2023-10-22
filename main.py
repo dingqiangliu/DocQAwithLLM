bc7df36935f3def360d5f5e4890dff5e7d0048b6
DQ <dingqiangliu@gmail.com>
2023-10-22 10:29:39 +0800

    Add questions loop.

--- main.py
+++ main.py
@@ -16,27 +16,39 @@ with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input',
+                        nargs='?',
                         type=str,
-                        default='How much is the minimum guarantee payable by adidas?',
+                        default='',
                         help='Enter the query to pass into the LLM')
     args = parser.parse_args()
 
     # Setup DBQA
-    start = timeit.default_timer()
     dbqa = setup_dbqa()
-    response = dbqa({'query': args.input})
-    end = timeit.default_timer()
-
-    print(f'\nAnswer: {response["result"]}')
-    print('='*50)
-
-    # Process source documents
-    source_docs = response['source_documents']
-    for i, doc in enumerate(source_docs):
-        print(f'\nSource Document {i+1}\n')
-        print(f'Source Text: {doc.page_content}')
-        print(f'Document Name: {doc.metadata["source"]}')
-        print(f'Page Number: {doc.metadata["page"]}\n')
-        print('='* 60)
-
-    print(f"Time to retrieve response: {end - start}")
+    
+    # query loop
+    query = args.input
+    if len(query) == 0:
+        query = input('Enter the query: ').strip() 
+    while query != '\\q':
+        start = timeit.default_timer()
+        response = dbqa({'query': query})
+        end = timeit.default_timer()
+    
+        print(f'\nAnswer: {response["result"]}')
+        print('='*50)
+    
+        # Process source documents
+        source_docs = response['source_documents']
+        for i, doc in enumerate(source_docs):
+            print(f'\nSource Document {i+1}\n')
+            print(f'Source Text: {doc.page_content}')
+            print(f'Document Name: {doc.metadata["source"]}')
+            print(f'Page Number: {doc.metadata["page"]}\n')
+            print('='* 60)
+        
+        print(f"Time to retrieve response: {end - start}")
+        
+        if len(args.input) > 0:
+            break
+        print('='* 80)
+        query = input('Enter the query: ')
