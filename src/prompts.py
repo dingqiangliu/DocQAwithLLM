7d1e8f289e7dbd824e9192929aa5b793922acd33
DQ <dingqiangliu@gmail.com>
2023-11-19 11:45:25 +0800

    fine tuning prompt

--- src/prompts.py
+++ src/prompts.py
@@ -7,7 +7,7 @@
 # as it is highly sensitive to whitespace changes. For example, it could have problems generating
 # a summary from the pieces of context if the spacing is not done correctly
 
-qa_template = """Use the following pieces of information to answer the user's question.
+qa_template = """Use the following pieces of information after Context to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 Context: {context}
